833dfddf85879bed86d0506e912a0b0eee47ce46
thembones79 <michal.szumnarski@gmail.com>
2019-12-28 13:30:17 +0100

    added bcrypt

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -5,6 +5,13 @@ exports.signup = function(req, res, next) {
   // console.log(req.body);
   const email = req.body.email;
   const password = req.body.password;
+
+  if (!email || !password) {
+    return res
+      .status(422)
+      .send({ error: "You must provide email and password!" });
+  }
+
   // See if a user with the given email exists
   User.findOne({ email: email }, function(err, existingUser) {
     if (err) {
